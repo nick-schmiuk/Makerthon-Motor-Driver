1c5adaaec8692bcfb9c8768dc7ea84bb84e2bc27
Nick <n.fastert@gmx.de>
2025-03-13 10:02:26 +0000

    Built basic motor driver

---
+++ motorHandler.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+class motor:
+    def __init__(self, pwmPin, forwardPin, backwardPin):
+        self.forwardPin = forwardPin
+        self.backwardPin = backwardPin
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+
+        GPIO.setup(pwmPin,GPIO.OUT)         # Throttle
+        GPIO.setup(forwardPin, GPIO.OUT)    # Forwards Direction
+        GPIO.output(forwardPin, GPIO.LOW)
+        GPIO.setup(backwardPin, GPIO.OUT)   # Backwards Direction
+        GPIO.output(backwardPin, GPIO.LOW)
+
+        self.pwm = GPIO.PWM(pwmPin, 1000)
+        # pwm.start(0)
+
+    def drive(self, speed, direction=0):
+        if speed == 0:
+            self.pwm.ChangeDutyCycle(0)
+            GPIO.output(self.forwardPin, GPIO.LOW)
+            GPIO.output(self.backwardPin, GPIO.LOW)
+            return
+
+        if direction == 1:
+            GPIO.output(self.forwardPin, GPIO.HIGH)
+            GPIO.output(self.backwardPin, GPIO.LOW)
+            self.pwm.ChangeDutyCycle(speed)
+            return
+        elif direction == -1:
+            GPIO.output(self.forwardPin, GPIO.LOW)
+            GPIO.output(self.backwardPin, GPIO.HIGH)
+            self.pwm.ChangeDutyCycle(speed)
+            return
+        
+        
+
