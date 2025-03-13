f388168d39b6e38cd9414672d26c9d9bee61643c
Nick <n.fastert@gmx.de>
2025-03-13 15:29:48 +0000

    Added probably not working code to handle the distance sensor

---
+++ distanceHandler.py
@@ -0,0 +1,25 @@
+import time
+import Adafruit_ADS1x15
+
+class ADS1115Reader:
+    def __init__(self, address=0x48, gain=1):
+        self.address = address
+        self.gain = gain
+        self.adc = Adafruit_ADS1x15.ADS1115(address)
+    
+    def read_voltage(self, channel):
+        # Read ADC value from the specified channel (0-3)
+        adc_value = self.adc.read_adc(channel, gain=self.gain)
+        # Convert the ADC value to voltage
+        voltage = adc_value * (4.096 / 32767.0)
+        return voltage
+
+if __name__ == "__main__":
+    # Create an instance of ADS1115Reader
+    ads_reader = ADS1115Reader()
+    
+    while True:
+        # Read voltage from channel 0
+        voltage = ads_reader.read_voltage(0)
+        print(f"Voltage: {voltage:.4f} V")
+        time.sleep(1)
