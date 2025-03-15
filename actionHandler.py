382e452f19af9f2dd67610583f7b01034e5722b0
Juan Pereiro González <jpereiro1@gmail.com>
2025-03-15 11:02:15 +0000

    fix speed

--- actionHandler.py
+++ actionHandler.py
@@ -74,16 +74,16 @@ class MainHandler:
             print("DETECT - GOING RIGHT")
             #self.motorL.drive(0)
             #self.motorR.drive(0)
-            self.motorL.drive(70, -1)
-            self.motorR.drive(20, 1)
+            self.motorL.drive(50, -1)
+            self.motorR.drive(10, 1)
             value = detectGarbage(picam2)
             return
         if value == -1:
             print("DETECT - GOING LEFT")
             #self.motorL.drive(0)
             #self.motorR.drive(0)
-            self.motorL.drive(20, 1)
-            self.motorR.drive(70, -1)
+            self.motorL.drive(10, 1)
+            self.motorR.drive(50, -1)
             value = detectGarbage(picam2)
             return
         if value == 0:
